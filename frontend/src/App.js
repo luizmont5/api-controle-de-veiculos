57314992fa7a6524ad973accb93c3fddf10a2f1f
luizmont5 <lfmr@outlook.com>
2024-08-20 17:02:08 -0300

    iniciando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+  <> 
+    <Header />
+    <h1>hello world</h1>
+    <Footer/>
+    </>
+  );
+  
+}
+
+export default App;
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from  "./Footer.module.css";
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>Bypass &copy; Developed by Luiz Ramalho</h2>
+            </footer>
+    )
+}
+
+export default Footer;
